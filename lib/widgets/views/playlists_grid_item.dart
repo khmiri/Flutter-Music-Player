a2e57a737cac46f80a993732188ccc3f342990f3
khmiri <devsonastyl@gmail.com>
2023-01-26 23:44:45 +0100

    adding offset to right items

--- lib/widgets/views/playlists_grid_item.dart
+++ lib/widgets/views/playlists_grid_item.dart
@@ -12,17 +12,22 @@ class PlaylistsGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
-    return Card(
-      shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(20))),
-      child: SizedBox(
-        height: 40,
-        width: 10,
-         child: ClipRRect(
-          borderRadius: const BorderRadius.all(Radius.circular(20)),
-          child: Image.asset(dummy.imageAssets[index])),
 
+    // using Transform to give staggered grid effect
+    // adding offset only for right column
+    return Transform.translate(
+      offset:(index%2!=0)?const Offset(0.0,30):Offset.zero,
+      child: Card(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20))),
+        child: SizedBox(
+          height: 40,
+          width: 10,
+           child: ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(20)),
+            child: Image.asset(dummy.imageAssets[index])),
+    
+        ),
       ),
     );
   }
